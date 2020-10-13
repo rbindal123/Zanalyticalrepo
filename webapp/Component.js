6a274e0f6ac0c93244dad9eeb0a90788e01e4c67
rbindal123 <bindalritu@outlook.com>
2020-10-13 15:25:07 +0000

    initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,13 @@
+sap.ui.define(["sap/ovp/app/Component"], function (Component) {
+	"use strict";
+	/**
+	 * @fileOverview Application component to display information on entities from the GWSAMPLE_BASIC
+	 *   OData service.
+	 * @version @version@
+	 */
+	return Component.extend("co.lb.ZANALYTICAL2.Component", {
+		metadata: {
+			manifest: "json"
+		}
+	});
+});
